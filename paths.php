f69ba70a2ecb41f4ba986abe0fa44515db8838b3
Davide Franco <bacula-dev@dflc.ch>
2010-10-25 21:47:03 +0200

    bacula-web: Adapted path for Smarty lib in paths.php

--- paths.php
+++ paths.php
@@ -6,7 +6,7 @@
 $phplot_path = "external_packages/phplot/";
 
 //Smarty package. version 2.6.10. http://smarty.php.net
-$smarty_path = "external_packages/smarty/";
+$smarty_path = "external_packages/smarty/libs/";
 
 //Smarty_gettext. version 0.9. http://www.boom.org.il/smarty/gettext/
 $smarty_gettext_path = "external_packages/smarty_gettext-0.9/"; 
