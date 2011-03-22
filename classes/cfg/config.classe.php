2bffd3f69a098bd42cdb1479f82e5b06a529eaf3
Davide Franco <bacula-dev@dflc.ch>
2011-03-22 15:53:47 +0100

    bacula-web: New classe BW_Config in config.classe.php

--- classes/cfg/config.classe.php
+++ classes/cfg/config.classe.php
@@ -1,29 +1,49 @@
 <?php
 
  class BW_Config {
+ 
+	private $config;
+	private $catalogs = array();
+	
 	function __construct()
 	{
-		
+			if( !$this->Check_Config_file() )
+				return true;
 	}
 	
 	public function Check_Config_file()
 	{
-	
+		// Check if config file exist and is readable
+		return is_readable( CONFIG_FILE );
 	}
 	
 	public function Get_Config()
 	{
+		$this->config = parse_ini_file( $this->config_file, true );
 		
+		if( !$this->config == false ) {
+			// Loading database connection information
+			foreach( $this->config as $parameter => $value )
+			{
+				if( is_array($value) )		// Parsing database section
+					array_push( $this->catalogs, $value );
+			}
+			return true;
+		}else
+			return false;		
 	}
 	
 	public function Get_Config_Params( $params = array() )
 	{
-	
+		if( isset( $this->config[$param] ) )
+			return $this->config[$param];
+		else
+			return false;
 	}
 	
 	public function Count_Catalogs()
 	{
-	
+		return count( $this->catalogs );
 	}
 	
 	public function Get_Dsn( $catalog_id )
@@ -33,7 +53,7 @@
 		
 	function __destruct()
 	{
-	
+		
 	}
 	
  } // end classe BW_Config
