ad86e12617795ee9da3b0980c1f8a22407c72c0b
Davide Franco <bacula-dev@dflc.ch>
2011-05-27 18:33:40 +0200

    bacula-web: Added job status code constant

--- config.inc.php
+++ config.inc.php
@@ -28,4 +28,24 @@
  define( 'LAST_WEEK', 604800 );
  define( 'LAST_MONTH', 2678400 );
  define( 'ALL', -1 );
+ 
+ // Job status code
+ define( 'J_NOT_RUNNING', 		  'C' );
+ define( 'J_RUNNING', 			  'R' );
+ define( 'J_BLOCKED', 			  'B' );
+ define( 'J_COMPLETED', 		  'T' );
+ define( 'J_COMPLETED_ERROR', 	  'E' );
+ define( 'J_NO_FATAL_ERROR', 	  'e' );
+ define( 'J_FATAL', 			  'f' );
+ define( 'J_CANCELED', 			  'A' );
+ define( 'J_WAITING_CLIENT', 	  'F' );
+ define( 'J_WAITING_SD', 		  'S' );
+ define( 'J_WAITING_NEW_MEDIA',	  'm' );
+ define( 'J_WAITING_MOUNT_MEDIA', 'M' );
+ define( 'J_WAITING_STORAGE_RES', 's' );
+ define( 'J_WAITING_JOB_RES',     'j' );
+ define( 'J_WAITING_CLIENT_RES',  'c' );
+ define( 'J_WAITING_MAX_JOBS',    'd' );
+ define( 'J_WAITING_START_TIME',  't' );
+ define( 'J_WAITING_HIGH_PR_JOB', 'p' );
 ?>
