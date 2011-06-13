31d1c747ed914ca425a26ae8ca353bc16ae9b8b1
Davide Franco <bacula-dev@dflc.ch>
2011-06-13 20:21:01 +0200

    bacula-web: Removed useless human_file_size function from bweb.inc.php

--- includes/bweb.inc.php
+++ includes/bweb.inc.php
@@ -112,41 +112,7 @@ class Bweb extends DB {
 	{
 		$this->db_link->disconnect();
     }      
-         
-	// Return humanized size with default unit of GB
-	// if auto provide for unit argument, automaticaly decide which unit
-	function human_file_size( $size, $decimal = 2, $unit = 'auto' )
-	{
-		$unit_id = 0;
-		$lisible = false;
-		$units = array('B','KB','MB','GB','TB');
-		$hsize = $size;
-
-		switch( $unit )
-		{
-			case 'auto';
-				while( !$lisible ) {
-					if ( $hsize >= 1024 ) {
-						$hsize    = $hsize / 1024;
-						$unit_id += 1;
-					}	 
-					else
-						$lisible = true;
-				} // end while
-			break;
-			
-			default:
-				$p = array_search( $unit, $units);
-				$hsize = $hsize / pow(1024,$p);
-			break;
-		} // end switch
-		
-		$hsize = sprintf("%." . $decimal . "f", $hsize);
-		$hsize = $hsize . ' ' . $units[$unit_id];
-		return $hsize;
-	} // end function
-
-	
+       
 	function GetDbSize() 
 	{
 		$database_size 	= 0;
