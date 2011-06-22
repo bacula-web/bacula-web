155105cef54e342868afe791f67cad0170382e5e
Davide Franco <bacula-dev@dflc.ch>
2011-06-22 17:43:33 +0200

    bacula-web: Split TimeUtils php class into new file timeutils.class.php

--- config.inc.php
+++ config.inc.php
@@ -33,6 +33,7 @@
  require_once BW_OBJ . "graph/bgraph.class.php";
  require_once BW_OBJ . "bweb.inc.php";
  require_once BW_OBJ . "utils/utils.class.php";
+ require_once BW_OBJ . "utils/timeutils.class.php";
  
  // Global constants
  define('CONFIG_DIR', BW_ROOT . "/config/");
--- includes/utils/timeutils.class.php
+++ includes/utils/timeutils.class.php
@@ -14,36 +14,39 @@
 | GNU General Public License for more details.                            |
 +-------------------------------------------------------------------------+ 
 */
-class Utils {
-	static public function Get_Human_Size( $size, $decimal = 2, $unit = 'auto' )
-	{
-		$unit_id = 0;
-		$lisible = false;
-		$units = array('B','KB','MB','GB','TB');
-		$hsize = $size;
 
-		switch( $unit )
-		{
-			case 'auto';
-				while( !$lisible ) {
-					if ( $hsize >= 1024 ) {
-						$hsize    = $hsize / 1024;
-						$unit_id += 1;
-					}	 
-					else
-						$lisible = true;
-				} // end while
-			break;
-			
-			default:
-				$p = array_search( $unit, $units);
-				$hsize = $hsize / pow(1024,$p);
-			break;
-		} // end switch
+class TimeUtils {
+	static public function Get_Elapsed_Time( $start_time, $end_time)
+	{
+		$start = '';
+		$end   = '';
 		
-		$hsize = sprintf("%." . $decimal . "f", $hsize);
-		$hsize = $hsize . ' ' . $units[$unit_id];
-		return $hsize;
+		if( $start_time == '0000-00-00 00:00:00' )
+			return 'N/A';
+		else
+			$start = strtotime( $start_time );					
+        
+		if( $end_time == '0000-00-00 00:00:00' )
+			$end = mktime();
+		else
+			$end   = strtotime( $end_time );
+		
+		$diff = $end - $start;
+
+        $daysDiff = sprintf("%02d", floor($diff/60/60/24) );
+        $diff -= $daysDiff*60*60*24;
+
+        $hrsDiff = sprintf("%02d", floor($diff/60/60) );
+        $diff -= $hrsDiff*60*60;
+
+        $minsDiff = sprintf("%02d", floor($diff/60) );
+        $diff -= $minsDiff*60;
+        $secsDiff = sprintf("%02d", $diff );
+
+        if( $daysDiff > 0 )
+			return $daysDiff . 'day(s) ' . $hrsDiff.':' . $minsDiff . ':' . $secsDiff;
+        else
+			return $hrsDiff . ':' . $minsDiff . ':' . $secsDiff;
 	}
 }
 
