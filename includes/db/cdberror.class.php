0e89b27f6917fb5d24c88ccc88dc3e6975d219b5
Davide Franco <bacula-dev@dflc.ch>
2011-07-17 21:59:15 +0200

    bacula-web: New raiseError() function in CDBError class

--- includes/db/cdberror.class.php
+++ includes/db/cdberror.class.php
@@ -1,23 +1,14 @@
 <?php
 
-class CDBError extends Exception
+class CDBError
 {
-	public function __construct( $message, $debug = false )
-	{
-		$this->debug_level 	= $debug;
-		$this->message     	= $message;
-		
-		$this->raiseError();
-		die();
-	}
-	
-	public function raiseError()
+	static public function raiseError( $exception )
 	{
 		echo '<h3>Dabase error: </h3>';
-		echo 'Message: ' . $this->getMessage() . '<br />';
-		echo 'Code: ' . $this->getCode()  . '<br />';
-		echo 'Line: ' . $this->getLine()  . '<br />';
-		echo 'File: ' . $this->getFile() . '<br />';
+		echo 'Message: ' . $exception->getMessage() . '<br />';
+		echo 'Code: ' . $exception->getCode()  . '<br />';
+		echo 'Line: ' . $exception->getLine()  . '<br />';
+		echo 'File: ' . $exception->getFile() . '<br />';
 	}
 }
 
