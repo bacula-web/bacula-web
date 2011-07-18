94930f8c4fe6c1de3ea066e75073a59ada73feda
Davide Franco <bacula-dev@dflc.ch>
2011-07-18 08:00:25 +0200

    bacula-web: New chhtp php class

---
+++ includes/utils/chttp.class.php
@@ -0,0 +1,46 @@
+<?php
+/* 
++-------------------------------------------------------------------------+
+| Copyright 2010-2011, Davide Franco			                          |
+|                                                                         |
+| This program is free software; you can redistribute it and/or           |
+| modify it under the terms of the GNU General Public License             |
+| as published by the Free Software Foundation; either version 2          |
+| of the License, or (at your option) any later version.                  |
+|                                                                         |
+| This program is distributed in the hope that it will be useful,         |
+| but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+| MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+| GNU General Public License for more details.                            |
++-------------------------------------------------------------------------+ 
+*/
+	class CHttp
+	{
+		private function getSafeValue( $value )
+		{
+			return $safe_value;
+		}
+		
+		// Return an array of $_POST or $_GET values
+		// If the value doesn't exist, the return value is FALSE
+		public static function getRequestVars( $type = 'POST', $vars = array() )
+		{
+			$values = array();
+			
+			if( count($vars) > 0 )
+				
+			
+			switch( $type )
+			{
+				case 'GET':
+
+				break;
+				case 'POST':
+
+				break;
+			}
+			return false;
+		}
+	}
+
+?>
