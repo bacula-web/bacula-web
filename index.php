57cbed37cd17edb33945f89e0b9038c4c47ac6e3
Davide Franco <bacula-dev@dflc.ch>
2012-05-20 19:51:42 +0200

    Updated php scripts header

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /* 
 +-------------------------------------------------------------------------+
 | Copyright (C) 2004 Juan Luis Francés Jiménez							  |
-| Copyright 2010-2011, Davide Franco			                          |
+| Copyright 2010-2012, Davide Franco			                          |
 |                                                                         |
 | This program is free software; you can redistribute it and/or           |
 | modify it under the terms of the GNU General Public License             |
