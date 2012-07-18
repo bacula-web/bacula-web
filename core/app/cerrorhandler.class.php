1176620af759466de6624d46b250a7f80b1eabdd
Davide Franco <bacula-dev@dflc.ch>
2012-07-18 11:21:20 +0200

    Removed unused raiseError() function from CErrorHandler class

--- core/app/cerrorhandler.class.php
+++ core/app/cerrorhandler.class.php
@@ -32,37 +32,6 @@ class CErrorHandler
 		self::$debug = $debug;
 	}
 	
-	public function raiseError()
-	{
-		// Display error
-		echo '<h3 style="background-color: #F0F0F0; width: 550px; padding: 5px; font-family: Arial,Verdana;">';
-		
-		if( !empty( self::$header ) )
-			echo self::$header;
-		else
-			echo 'Application error';
-		
-		echo '</h3>';
-		
-		// Show more information if debug mode is enabled
-		echo '<p style="width: 550px; padding: 5px; font-family: Arial,Verdana; font-size: 10pt;">';
-		echo 'Message: ' . $this->getMessage() . '<br />';
-		if( self::$debug ) {
-			echo 'Code: ' . $this->getCode()  . '<br />';	
-			echo 'Line: ' . $this->getLine()  . '<br />';
-			echo 'File: ' . $this->getFile() . '</p>';
-		}
-		
-		// Display footer
-		$footer  = '<p style="font-size: 10pt; background-color: #F0F0F0; width: 550px; padding: 5px; font-family: Arial,Verdana;">';
-		$footer .= 'Tried to run the <a href="test.php">test page</a> ?<br />';
-		$footer .= 'Read the documentation on the <a href="http://bacula-web.dflc.ch" target="_blank">Bacula-Web project site</a> <br />';
-		$footer .= 'Rebort a bug or suggest a new feature in the <a href="http://bacula-web.dflc.ch/bugs" target="_blank">Bacula-Web\'s bugtracking tool</a> <br /> </p>';
-		echo $footer;
-		
-		//die();
-	}
-	
 	public static function displayError( $exception )
 	{
 		switch( get_class($exception) )
