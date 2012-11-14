aca34ed555e003faf95c188eff351d1b36aadfef
Davide Franco <bacula-dev@dflc.ch>
2012-11-14 14:24:18 +0100

    Exception error message improvments
     - updated css code for error message (use the more width space)
     - fixed english typo in footer

--- core/app/cerrorhandler.class.php
+++ core/app/cerrorhandler.class.php
@@ -42,9 +42,9 @@ class CErrorHandler {
                 break;
         } // end switch
 
-        echo '<h3>' . self::$header . '</h3>';
-        echo '<p style="width: 550px; padding: 5px; font-family: Arial,Verdana; font-size: 10pt;">';
-        echo 'Message: ' . $exception->getMessage() . '<br />';
+        echo '<h4 style="padding: 0px 5px; font-family:Arial,Verdana;">' . self::$header . '</h4>';
+        echo '<p style="width: 1000px; padding: 0px 5px; font-family: Arial,Verdana; font-size: 10pt;">';
+        echo '<b>Message:</b> ' . $exception->getMessage() . '<br />';
 
         // Show more information is debug mode is enabled
         if (self::$debug) {
@@ -54,9 +54,9 @@ class CErrorHandler {
         }
 
         // Display footer
-        $footer = '<p style="font-size: 10pt; background-color: #F0F0F0; width: 550px; padding: 5px; font-family: Arial,Verdana;">';
-        $footer .= 'Tried to run the <a href="test.php">test page</a> ?<br />';
-        $footer .= 'Read the documentation on the <a href="http://www.bacula-web.org" target="_blank">Bacula-Web project site</a> <br />';
+        $footer = '<p style="font-size: 10pt; background-color: #F0F0F0; width: 1000px; padding: 10px; font-family: Arial,Verdana;">';
+        $footer .= 'Have you try to run the <a href="test.php">test page</a> ?<br />';
+        $footer .= 'Check the online documentation on <a href="http://www.bacula-web.org" target="_blank">Bacula-Web project site</a> <br />';
         $footer .= 'Rebort a bug or suggest a new feature in the <a href="http://www.bacula-web.org/bugs" target="_blank">Bacula-Web\'s bugtracking tool</a> <br /> </p>';
         echo $footer;
 
