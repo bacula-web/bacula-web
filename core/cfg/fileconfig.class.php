fbdd8d9e3fe749f661906d438153f44e1caa8922
Davide Franco <bacula-dev@dflc.ch>
2012-12-05 08:09:25 +0100

    New static open() function in FileConfig class
     - the open() function check if the config file is readable by Apache and then, make sure that at least one database connection have been defined in the configuration file

--- core/cfg/fileconfig.class.php
+++ core/cfg/fileconfig.class.php
@@ -17,4 +17,47 @@
 
  class FileConfig {
 
- }
+	static private $config_parameters;
+	
+    // ==================================================================================
+	// Function: 	__constructor()
+	// Parameters:	none
+	// Return:		none
+	// ==================================================================================
+	
+	public function __construct() {
+	
+	}
+	
+    // ==================================================================================
+	// Function: 	open()
+	// Parameters:	none
+	// Return:		return false if the file is unreadable (not found or no enough permission) or no db connection defined
+	// ==================================================================================
+	
+	static public function open() {
+	
+		// Check if config file exist and is readable, then include it
+        if ( is_readable( CONFIG_FILE ) ) {
+			require_once( CONFIG_FILE );
+        }else {
+            throw new Exception("Config file not found or bad file permissions");
+            return false;
+        }
+		
+		// Getting global $config variable
+		global $config;
+		var_dump($config);
+		self::$config_parameters = $config;
+
+        // Checking options and database parameters
+        if ( !array_key_exists('0', self::$config_parameters) ) {
+            throw new Exception("At least one catalog should be defined in the configuration");
+            return false;
+        }
+	
+		return true;
+	}
+	
+	
+ } // end class
