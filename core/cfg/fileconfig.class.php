f51ec67547dfef75257183bf58bd421d7ceb85da
Davide Franco <bacula-dev@dflc.ch>
2012-12-06 19:07:35 +0100

    New get_Value() function in FileConfig class
     - simply return the provided in arguments parameter value
     - an optional catalog_id argument can be provided for the case that there's several catalogs (the first one is used by default)

--- core/cfg/fileconfig.class.php
+++ core/cfg/fileconfig.class.php
@@ -59,5 +59,31 @@
 			return false;
 		}
 	} // end function open()
+
+ 	// ==================================================================================
+	// Function: 	get_Value()
+	// Parameters:	$parameter
+	//				$catalog_id (optional), take the first catalog by default
+	// Return:		Database size
+	// ==================================================================================
+	
+	public static function get_Value( $parameter, $catalog_id = null ) {
+		// Check if the $global_config have been already set first
+		if( !isset(FileConfig::$global_config) ) {
+			throw new Exception("The configuration is missing or ther's something wrong in it");
+			return false;
+		}
+		
+		// if the catalog id have been defined in parameters
+		if( !is_ntull($catalog_id) ) {
+			if( isset( FileConfig::$global_config[$catalog_id][$parameter] ) ) {
+			}else{
+				throw new Exception("The parameter $parameter is missing in the configuration");
+				return 
+			}
+		}
+	} // end function
+	
 	
  } // end class
+ ?>
