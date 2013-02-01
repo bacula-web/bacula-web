6cbdb55815376f06e4705a96e7eba186bff45bf4
Davide Franco <bacula-dev@dflc.ch>
2013-02-01 13:14:49 +0100

    Improvements in CErrorHandler php class
     - added filename and line number of the exception
     - removed unused variables and code in displayError() function
     - exception information displayed in a html table from now

--- core/app/cerrorhandler.class.php
+++ core/app/cerrorhandler.class.php
@@ -19,16 +19,10 @@
 class CErrorHandler {
 
     private static $header;
-    private static $debug;
 
     // Define Header
     public function setHeader($header) {
         self::$header = $header;
-    }
-
-    // Enable debug mode
-    public function setDebug($debug = true) {
-        self::$debug = $debug;
     }
 
     public static function displayError($exception) {
@@ -41,25 +35,29 @@ class CErrorHandler {
                 self::setHeader('Application error');
                 break;
         } // end switch
-
-        echo '<h4 style="padding: 0px 5px; font-family:Arial,Verdana;">' . self::$header . '</h4>';
-        echo '<p style="width: 1000px; padding: 0px 5px; font-family: Arial,Verdana; font-size: 10pt;">';
-        echo '<b>Message:</b> ' . $exception->getMessage() . '<br />';
-
-        // Show more information is debug mode is enabled
-        if (self::$debug) {
-            echo 'Code: ' . $exception->getCode() . '<br />';
-            echo 'Line: ' . $exception->getLine() . '<br />';
-            echo 'File: ' . $exception->getFile() . '</p>';
-        }
-
-        // Display footer
-        $footer = '<p style="font-size: 10pt; background-color: #F0F0F0; width: 1000px; padding: 10px; font-family: Arial,Verdana;">';
-        $footer .= 'Have you try to run the <a href="test.php">test page</a> ?<br />';
-        $footer .= 'Check the online documentation on <a href="http://www.bacula-web.org" target="_blank">Bacula-Web project site</a> <br />';
-        $footer .= 'Rebort a bug or suggest a new feature in the <a href="http://www.bacula-web.org/bugs" target="_blank">Bacula-Web\'s bugtracking tool</a> <br /> </p>';
-        echo $footer;
-
+		
+		$output  = '<table style="margin: 10px; width: 900px; border: 1px solid #c0c0c0;">';
+		
+		// Header
+		$output .= "<tr> <th colspan='2'>" . self::$header . "</th> </tr>";
+		
+		// Exception details
+		$output .= "<tr> <td class='left_align' width='200'>File</td> <td class='left_align'>" . $exception->getFile() . "</td> \n</tr> \n";
+		$output .= "<tr> <td class='left_align'>Line</td> <td class='left_align'>" . $exception->getLine() . "</td> \n </tr> \n";
+		$output .= "<tr> <td class='left_align'>Exception code</td> <td class='left_align'>" . $exception->getCode() . "</td> \n </tr> \n";
+		$output .= "<tr> <td class='left_align'>Exception message</td> <td class='left_align'>" . $exception->getMessage() . "</td> \n </tr> \n";
+		
+		$output .= "<tfoot> \n <tr> \n";
+		$output .= "<td class='left_align' colspan='2'> \n";
+		$output .= "Have you try to run the <a href='test.php'>test page</a> ?<br />";
+		$output .= "Check the online documentation on <a href='http://www.bacula-web.org' target='_blank'>Bacula-Web project site</a> <br />";
+		$output .= "Rebort a bug or suggest a new feature in the <a href='http://bugs.bacula-web.org' target='_blank'>Bacula-Web\'s bugtracking tool</a> <br />";
+		$output .= "</td> \n";
+		$output .= " \n</tr> \n </tfoot>";
+		
+		$output .= "</table>";
+		
+		echo $output;
         die();
     }
 
