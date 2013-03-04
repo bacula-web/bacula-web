9c8a8b8c7cc26a810a0546f1a8c0ecbb547a308b
Davide Franco <bacula-dev@dflc.ch>
2013-03-04 10:53:47 +0100

    PHP class autoload implementation
     - new ClassAutoLoader class
     - removed useless require_once() function call in global.inc.php

---
+++ core/app/classautoloader.class.php
@@ -0,0 +1,126 @@
+<?php
+ /*
+  +-------------------------------------------------------------------------+
+  | Copyright 2010-2013, Davide Franco                                              |
+  |                                                                         |
+  | This program is free software; you can redistribute it and/or           |
+  | modify it under the terms of the GNU General Public License             |
+  | as published by the Free Software Foundation; either version 2          |
+  | of the License, or (at your option) any later version.                  |
+  |                                                                         |
+  | This program is distributed in the hope that it will be useful,         |
+  | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+  | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+  | GNU General Public License for more details.                            |
+  +-------------------------------------------------------------------------+
+ */
+
+class ClassAutoLoader {
+
+  public static $loader;
+  private static $paths;
+  private static $exclusion;
+
+  // ==================================================================================
+  // Function: 	    __construct()
+  // Parameters:    none
+  // Return:	    
+  // ==================================================================================
+
+  public function __construct() {
+    self::$paths     = array();
+    self::$exclusion = array();
+    
+    spl_autoload_register( array($this,'Load_Class'), true );
+    spl_autoload_register( array($this,'Load_Models'), true );
+  }
+
+  // ==================================================================================
+  // Function: 	    init()
+  // Parameters:    none
+  // Return:	    ClassAutoLoader object
+  // ==================================================================================
+
+  public static function init()
+  {
+    if( self::$loader == NULL )
+      self::$loader = new self();
+
+    return self::$loader;
+  }    
+
+  // ==================================================================================
+  // Function: 	    add_Path()
+  // Parameters:    $pathname
+  // Return:	    
+  // ==================================================================================
+
+  public function add_Path($pathname) {
+    // Scan and add all subfolders
+    if( file_exists( $pathname ) )
+        self::$paths = array_merge(self::$paths, self::scan_Path( $pathname ) );    
+  }
+  
+  // ==================================================================================
+  // Function: 	    add_Exclusion()
+  // Parameters:    $path
+  // Return:	    
+  // ==================================================================================
+
+  public function add_Exclusion($path) {
+    self::$exclusion[] = $path;
+  }
+  
+  // ==================================================================================
+  // Function: 	    Scan_Path()
+  // Parameters:    $path
+  // Return:	    array with containing folder and subfolder(s)
+  // ==================================================================================
+
+  public function scan_Path( $path ) {
+    $cf   = null;
+    $cf[] = $path;
+    
+    foreach( glob($path.'/*', GLOB_ONLYDIR) as $dir) {
+      foreach(self::scan_Path($dir) as $sf ) {
+        if( !in_array($sf, self::$exclusion) )
+            $cf[] = $sf;
+      }
+    }
+    return $cf;
+  }
+
+  // ==================================================================================
+  // Function: 	    Load_Class()
+  // Parameters:    $classname
+  // Return:	    
+  // ==================================================================================
+  
+  private function Load_Class($classname) {
+    foreach( self::$paths as $dir ) {      
+      $file_full_path = $dir . '/' . $classname . '.class.php';
+      $file_full_path = strtolower($file_full_path);
+      
+      if( file_exists( $file_full_path ) )
+        include( $file_full_path );
+    }
+  }
+  
+  // ==================================================================================
+  // Function: 	    Load_Models()
+  // Parameters:    $classname
+  // Return:	    
+  // ==================================================================================
+        
+  public function Load_Models( $classname ) {
+    foreach( self::$paths as $dir ) {      
+      list($class) = explode('_', $classname);
+      $file_full_path = $dir . '/' . $class . '.model.php';
+      $file_full_path = strtolower($file_full_path);
+            
+      if( file_exists( $file_full_path ) )
+        include( $file_full_path );
+    }    
+  }
+
+} // end class ClassAutoLoader
--- core/global.inc.php
+++ core/global.inc.php
@@ -16,105 +16,69 @@
   +-------------------------------------------------------------------------+
  */
 
-define('BW_ROOT', getcwd());
-define('BW_OBJ', BW_ROOT . '/core/');
-define('BW_LIBS', BW_ROOT . '/application/libs/');
-define('BW_EXTERNAL', BW_OBJ . 'external');
+ // Class autloader initialization
+ require_once(getcwd() . "/core/app/classautoloader.class.php");
+ ClassAutoLoader::init();
+ 
+ // Path exclusion
+ ClassAutoLoader::add_Exclusion('core/external/smarty-gettext');
+ 
+ // Add paths
+ ClassAutoLoader::add_Path('core');
+ ClassAutoLoader::add_Path('application');
+ 
+ // Views path
+ define('BW_ROOT', getcwd());
+ define('VIEW_DIR', BW_ROOT . "/application/view/");
+ define('VIEW_CACHE_DIR', "application/view/cache");
+ 
+ // Configuration
+ define('CONFIG_DIR', BW_ROOT . "/application/config/");
+ define('CONFIG_FILE', CONFIG_DIR . "config.php");
+ 
+ // Locales
+ define('LOCALE_DIR', BW_ROOT . '/application/locale');
+ 
+ // Smarty
+ require_once(BW_ROOT . '/core/external/smarty/libs/Smarty.class.php');
+ require_once(BW_ROOT . '/core/external/smarty-gettext/smarty_gettext.php');
+ 
+ // PHPLot
+ require_once(BW_ROOT . '/core/external/phplot/phplot.php');
+ 
+ // Time intervals in secondes
+ define('FIRST_DAY', mktime(0, 0, 0, 1, 1, 1970));
+ define('NOW', time());
+ define('LAST_DAY', NOW - 86400);
+ define('LAST_WEEK', NOW - 604800);
+ define('LAST_MONTH', NOW - 2678400);
+ define('DAY', 86400);
+ define('WEEK', 604800);
+ define('MONTH', 2678400);
+ define('ALL', -1);
 
-define('BW_PHPLOT', BW_EXTERNAL . '/phplot/');
-define('BW_SMARTY', BW_EXTERNAL . '/smarty/libs/');
-define('BW_SMARTY_GETTEXT', BW_EXTERNAL . '/smarty-gettext/');
+ // Job status code
+ define('J_NOT_RUNNING', 'C');
+ define('J_RUNNING', 'R');
+ define('J_BLOCKED', 'B');
+ define('J_COMPLETED', 'T');
+ define('J_COMPLETED_ERROR', 'E');
+ define('J_NO_FATAL_ERROR', 'e');
+ define('J_FATAL', 'f');
+ define('J_CANCELED', 'A');
+ define('J_WAITING_CLIENT', 'F');
+ define('J_WAITING_SD', 'S');
+ define('J_WAITING_NEW_MEDIA', 'm');
+ define('J_WAITING_MOUNT_MEDIA', 'M');
+ define('J_WAITING_STORAGE_RES', 's');
+ define('J_WAITING_JOB_RES', 'j');
+ define('J_WAITING_CLIENT_RES', 'c');
+ define('J_WAITING_MAX_JOBS', 'd');
+ define('J_WAITING_START_TIME', 't');
+ define('J_WAITING_HIGH_PR_JOB', 'p');
 
-// Configuration
-define('CONFIG_DIR', BW_ROOT . "/application/config/");
-define('CONFIG_FILE', CONFIG_DIR . "config.php");
-
-// Main application
-require_once BW_OBJ . "bweb.inc.php";
-
-// Template engine
-require_once( BW_SMARTY . "Smarty.class.php");
-require_once( BW_SMARTY_GETTEXT . "smarty_gettext.php" );
-
-// Graph
-require_once( BW_PHPLOT . "phplot.php");
-require_once BW_OBJ . "graph/cgraph.class.php";
-
-// Utils
-require_once BW_OBJ . "utils/cutils.class.php";
-require_once BW_OBJ . "utils/ctimeutils.class.php";
-require_once BW_OBJ . "utils/file.class.php";
-
-// Configuration 
-require_once BW_LIBS . "fileconfig.class.php";
-
-// Http request controler
-require_once BW_OBJ . "utils/chttprequest.class.php";
-
-// Exception handler
-require_once BW_OBJ . "app/cerrorhandler.class.php";
-
-// MVC
-require_once BW_OBJ . "app/cview.class.php";
-
-// Database abstraction
-require_once BW_OBJ . "db/cdb.class.php";
-require_once BW_OBJ . "db/cdbresult.class.php";
-require_once BW_OBJ . "db/cdbquery.class.php";
-require_once BW_OBJ . "db/cdbutils.class.php";
-
-// Models
-require_once BW_OBJ . "app/cmodel.class.php";
-require_once('application/models/jobs.model.php');
-require_once('application/models/clients.model.php');
-require_once('application/models/database.model.php');
-require_once('application/models/filesets.model.php');
-require_once('application/models/pools.model.php');
-require_once('application/models/volumes.model.php');
-
-// I18n (Internationalization)
-require_once BW_OBJ . "i18n/ctranslation.class.php";
-
-// Views
-define('VIEW_DIR', BW_ROOT . "/application/view/");
-define('VIEW_CACHE_DIR', "application/view/cache");
-
-// Locale
-define('LOCALE_DIR', BW_ROOT . '/application/locale');
-
-// Time intervals in secondes
-define('FIRST_DAY', mktime(0, 0, 0, 1, 1, 1970));
-define('NOW', time());
-define('LAST_DAY', NOW - 86400);
-define('LAST_WEEK', NOW - 604800);
-define('LAST_MONTH', NOW - 2678400);
-define('DAY', 86400);
-define('WEEK', 604800);
-define('MONTH', 2678400);
-define('ALL', -1);
-
-// Job status code
-define('J_NOT_RUNNING', 'C');
-define('J_RUNNING', 'R');
-define('J_BLOCKED', 'B');
-define('J_COMPLETED', 'T');
-define('J_COMPLETED_ERROR', 'E');
-define('J_NO_FATAL_ERROR', 'e');
-define('J_FATAL', 'f');
-define('J_CANCELED', 'A');
-define('J_WAITING_CLIENT', 'F');
-define('J_WAITING_SD', 'S');
-define('J_WAITING_NEW_MEDIA', 'm');
-define('J_WAITING_MOUNT_MEDIA', 'M');
-define('J_WAITING_STORAGE_RES', 's');
-define('J_WAITING_JOB_RES', 'j');
-define('J_WAITING_CLIENT_RES', 'c');
-define('J_WAITING_MAX_JOBS', 'd');
-define('J_WAITING_START_TIME', 't');
-define('J_WAITING_HIGH_PR_JOB', 'p');
-
-// Job levels
-define('J_FULL', 'F');
-define('J_DIFF', 'D');
-define('J_INCR', 'I');
+ // Job levels
+ define('J_FULL', 'F');
+ define('J_DIFF', 'D');
+ define('J_INCR', 'I');
 ?>
