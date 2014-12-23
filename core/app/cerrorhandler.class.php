3e194d0c826084daaac883bfbc24e87968626fe8
Davide Franco <bacula-dev@dflc.ch>
2014-12-23 16:35:35 +0100

    Code cleanup in CErrorHandler class

--- core/app/cerrorhandler.class.php
+++ core/app/cerrorhandler.class.php
@@ -37,17 +37,17 @@ class CErrorHandler {
 		$output .= self::getFormatedTrace($exception);
 		
 		// Header
-		$output .= '<table style="margin: 10px; width: 900px; border: 1px solid #c0c0c0;">';
-		$output .= "<tr> <th class='left_align' colspan='2'>" . self::$header . "</th> \n </tr> \n";
+		$output .= '<table>';
+		$output .= "<tr> <th colspan='2'>" . self::$header . "</th> \n </tr> \n";
 		
 		// Exception details
-		$output .= "<tr> <td class='left_align' width='200'><b>File</b> </td> <td class='left_align'>" . $exception->getFile() . "</td> \n</tr> \n";
-		$output .= "<tr> <td class='left_align'><b>Line</b> </td> <td class='left_align'>" . $exception->getLine() . "</td> \n </tr> \n";
-		$output .= "<tr> <td class='left_align'><b>Exception code</b> </td> <td class='left_align'>" . $exception->getCode() . "</td> \n </tr> \n";
-		$output .= "<tr> <td class='left_align'><b>Exception message</b> </td> <td class='left_align'>" . $exception->getMessage() . "</td> \n </tr> \n";
+		$output .= "<tr> <td width='200'><b>File</b> </td> <td>" . $exception->getFile() . "</td> \n</tr> \n";
+		$output .= "<tr> <td><b>Line</b> </td> <td>" . $exception->getLine() . "</td> \n </tr> \n";
+		$output .= "<tr> <td><b>Exception code</b> </td> <td>" . $exception->getCode() . "</td> \n </tr> \n";
+		$output .= "<tr> <td><b>Exception message</b> </td> <td>" . $exception->getMessage() . "</td> \n </tr> \n";
 		
 		$output .= "<tfoot> \n <tr> \n";
-		$output .= "<td class='left_align' colspan='2'> \n";
+		$output .= "<td colspan='2'> \n";
 		$output .= "Have you try to run the <a href='test.php'>test page</a> ?<br />";
 		$output .= "Check the online documentation on <a href='http://www.bacula-web.org' target='_blank'>Bacula-Web project site</a> <br />";
 		$output .= "Rebort a bug or suggest a new feature in the <a href='http://bugs.bacula-web.org' target='_blank'>Bacula-Web\'s bugtracking tool</a> <br />";
@@ -62,34 +62,22 @@ class CErrorHandler {
 	
 	public static function getFormatedTrace( $e ) {
 		$formated_trace  = '<table style="margin: 10px; width: 900px; border: 1px solid #c0c0c0;">';
-		$formated_trace .= '<tr> <th class="left_align">Exception trace</th> </tr>';
+		$formated_trace .= '<tr> <th>Exception trace</th> </tr>';
 		
 		foreach( $e->getTrace() as $exception ) {
-		
-		//	foreach( $exception as $key => $trace) {
 			$formated_trace .= '<tr>';
-			$formated_trace .= '<td class="left_align">';
+			$formated_trace .= '<td>';
 			$formated_trace .= 'File: <b>' . $exception['file'] . '</b> ';
 			$formated_trace .= 'on line <b>' . $exception['line'] . '</b> ';
 			$formated_trace .= 'in function <b>' . $exception['class'] . $exception['type'] . $exception['function'] . '</b>';
 			$formated_trace .= '</td>';
-
-			// Function arguments
-			//$formated_trace .= '<td>';
-			//foreach( $exception['args'] as $param )
-			//	$formated_trace .= $param;
-			//$formated_trace .= '</td>';
-
 			$formated_trace .= '</tr>';
-		//	}
 		}
 
-			
 		$formated_trace .= '</table>';
 		
 		return $formated_trace;
 	}
-
 }
 ?>
 
