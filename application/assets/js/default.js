02f0c17be865764a6030c02f60dc03896a8b2d6a
Davide Franco <bacula-dev@dflc.ch>
2015-01-15 17:55:10 +0100

    Add JavaScript code to delay graph images display

--- application/assets/js/default.js
+++ application/assets/js/default.js
@@ -1,5 +1,11 @@
-/* Bootstrap tooltip */
-
 $(document).ready(function(){
+    /* Bootstrap Tooltip */
     $('[data-toggle="tooltip"]').tooltip();
-});
+
+    /* Hide graph images for few milliseconds */
+    $("img").hide();
+    /* The code below does not work :( 
+    $(".img_loader").delay(1000).fadeOut(500);
+    */
+    $("img").delay(500).fadeIn('slow');
+});           
