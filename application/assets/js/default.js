aa5b9e6234940dc6997ea5e1544d2059d214e95e
Davide Franco <bacula-dev@dflc.ch>
2015-01-16 14:10:10 +0100

    Move graph loader code on top of the script and update elements animation delay

--- application/assets/js/default.js
+++ application/assets/js/default.js
@@ -1,11 +1,9 @@
 $(document).ready(function(){
-    /* Bootstrap Tooltip */
-    $('[data-toggle="tooltip"]').tooltip();
-
     /* Hide graph images for few milliseconds */
     $("img").hide();
-    /* The code below does not work :( 
-    $(".img_loader").delay(1000).fadeOut(500);
-    */
-    $("img").delay(500).fadeIn('slow');
+    $(".img_loader").delay(200).fadeOut(300);
+    $("img").delay(500).fadeIn(600);
+    
+    /* Bootstrap Tooltip */
+    $('[data-toggle="tooltip"]').tooltip();
 });           
