bcc7ba2e94aa7a2258c9ac2195a66c6e0ec18c91
Davide Franco <bacula-dev@dflc.ch>
2017-06-11 12:11:46 +0200

    Add new volumes report page
     - split previous <Pools And Volumes> report page
     - you can click on <Show volumes> on each pool to see all volumes associated with this pool
     - removed accordion as it does not make sense anymore

---
+++ application/view/volumes.tpl
@@ -0,0 +1,44 @@
+{include file=header.tpl}
+
+<div class="container">
+
+  <div class="row">
+
+    <div class="col-xs-12">
+
+      <h3>{$page_name}</h3>
+
+      <div class="table-responsive">
+        <table class="table table-condensed table-striped text-center">
+          <tr>
+	    <th class="text-center">{t}Volume name{/t}</th>
+	    <th class="text-center">{t}Bytes{/t}</th>
+	    <th class="text-center">{t}Media Type{/t}</th>
+	    <th class="text-center">{t}Expire{/t}</th>
+	    <th class="text-center">{t}Last written{/t}</th>
+	    <th class="text-center">{t}Status{/t}</th>
+	    <th class="text-center">{t}Slot{/t}</th>
+	    <th class="text-center">{t}In Changer{/t}</th>
+	  </tr>
+	  {foreach from=$volumes item=volume name=volumes}
+            <tr>
+	      <td>{$volume.volumename}</td>
+	      <td>{$volume.volbytes}</td>
+	      <td>{$volume.mediatype}</td>
+	      <td>{$volume.expire}</td>
+	      <td>{$volume.lastwritten}</td>
+	      <td>{$volume.volstatus}</td>
+	      <td>{$volume.slot}</td>
+	      <td>{$volume.inchanger}</td>
+	    </tr>
+	  {/foreach}
+        </table>
+      </div> <!-- end div class="table-responsive" -->
+    </div> <!-- end div class=col-xxx -->
+  </div> <!-- end div class="row" -->
+
+</div> <!-- div class="container-fluid" -->
+
+<!-- End pools.tpl -->
+
+{include file="footer.tpl"}
---
+++ volumes.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ +-------------------------------------------------------------------------+
+ | Copyright 2010-2017, Davide Franco			                   |
+ |                                                                         |
+ | This program is free software; you can redistribute it and/or           |
+ | modify it under the terms of the GNU General Public License             |
+ | as published by the Free Software Foundation; either version 2          |
+ | of the License, or (at your option) any later version.                  |
+ |                                                                         |
+ | This program is distributed in the hope that it will be useful,         |
+ | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+ | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+ | GNU General Public License for more details.                            |
+ +-------------------------------------------------------------------------+
+*/
+
+ session_start();
+ include_once('core/global.inc.php');
+
+ // Initialise view and model
+ $view = new CView();
+ $dbSql = new Bweb($view);
+
+ $volumes = new Volumes_Model();
+ $volumes_list = array();
+
+ foreach ($volumes->getVolumes() as $volume) {
+     // Set lastwritten for the volume
+   if (empty($volume['lastwritten'])) {
+       $volume['lastwritten'] = 'n/a';
+   } else {
+       // Format lastwritten in custom format if defined in config file
+     if (FileConfig::get_Value('datetime_format') != false) {
+         $volume['lastwritten'] = date(FileConfig::get_Value('datetime_format'), strtotime($volume['lastwritten']));
+     }
+   }
+
+   // Get volume used bytes in a human format
+   $volume['volbytes'] = CUtils::Get_Human_Size($volume['volbytes']);
+
+   // Update volume inchanger
+   if ($volume['inchanger'] == '0') {
+       $volume['inchanger'] = '-';
+   } else {
+       $volume['inchanger'] = '<span class="glyphicon glyphicon-ok"></span>';
+   }
+
+   // If volume have already been used
+   if ($volume['lastwritten'] != "0000-00-00 00:00:00") {
+       // Calculate expiration date if the volume status is Full or Used
+     if ($volume['volstatus'] == 'Full' || $volume['volstatus'] == 'Used') {
+         $expire_date = strtotime($volume['lastwritten']) + $volume['volretention'];
+         $volume['expire'] = strftime("%Y-%m-%d", $expire_date);
+     } else {
+         $volume['expire'] = 'n/a';
+     }
+   } else {
+       $volume['expire'] = 'n/a';
+   }
+
+   // add volume in volumes list array
+   $volumes_list[] = $volume;
+ }
+
+ $view->assign('volumes', $volumes_list);
+
+ // Set page name
+ $current_page = 'Volumes report';
+ $view->assign('page_name', $current_page);
+
+ // Process and display the template
+ $view->display('volumes.tpl');
