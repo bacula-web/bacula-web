e71ada8d893023276cbd0d8c0f5daed9cf9cfe54
Davide Franco <bacula-dev@dflc.ch>
2017-09-20 08:48:30 +0200

    Fix code indentation

--- jobs.php
+++ jobs.php
@@ -13,41 +13,40 @@
   | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
   | GNU General Public License for more details.                            |
   +-------------------------------------------------------------------------+
-*/
-  session_start();
-  require_once('core/global.inc.php');
+ */
 
-  try {
+session_start();
+require_once('core/global.inc.php');
 
-  $view = new CView();
-  $dbSql = new Bweb($view);
-  $jobs = new Jobs_Model();  
+try {
+   $view = new CView();
+   $dbSql = new Bweb($view);
+   $jobs = new Jobs_Model();  
+   
+   // That's horrible, it must be improved
+   require_once('core/const.inc.php');
+   
+   // Set tatal jobs first
+   $view->assign('total_jobs', $jobs->count());
 
-  // That's horrible, it must be improved
-  require_once('core/const.inc.php');
+   // Levels list filter
+   $levels_list = $jobs->getLevels($job_levels);
+   $levels_list['']  = 'Any';
+   $view->assign('levels_list', $levels_list);
 
-  // Set tatal jobs first
-  $view->assign('total_jobs', $jobs->count());
+   $query     = "";
+   $last_jobs = array();
 
-  // Levels list filter
-  $levels_list = $jobs->getLevels($job_levels);
-  $levels_list['']  = 'Any';
-  $view->assign('levels_list', $levels_list);
+   // Job Status list
+   define('STATUS_ALL', 0);
+   define('STATUS_RUNNING', 1);
+   define('STATUS_WAITING', 2);
+   define('STATUS_COMPLETED', 3);
+   define('STATUS_COMPLETED_WITH_ERRORS', 4);
+   define('STATUS_FAILED', 5);
+   define('STATUS_CANCELED', 6);
 
-  $query     = "";
-  $last_jobs = array();
-
-  // Job Status list
-  define('STATUS_ALL', 0);
-  define('STATUS_RUNNING', 1);
-  define('STATUS_WAITING', 2);
-  define('STATUS_COMPLETED', 3);
-  define('STATUS_COMPLETED_WITH_ERRORS', 4);
-  define('STATUS_FAILED', 5);
-  define('STATUS_CANCELED', 6);
-
-  $job_status = array(
-      STATUS_ALL => 'All',
+   $job_status = array( STATUS_ALL => 'All',
       STATUS_RUNNING => 'Running',
       STATUS_WAITING => 'Waiting',
       STATUS_COMPLETED => 'Completed',
@@ -59,27 +58,26 @@
   $view->assign('job_status', $job_status);
 
   // Global variables
-  $job_levels = array(
-      'D' => 'Differential',
-      'I' => 'Incremental',
-      'F' => 'Full',
-      'V' => 'InitCatalog',
-      'C' => 'Catalog',
-      'O' => 'VolumeToCatalog',
-      'd' => 'DiskToCatalog',
-      'A' => 'Data'
-   );
+  $job_levels = array( 'D' => 'Differential',
+     'I' => 'Incremental',
+     'F' => 'Full',
+     'V' => 'InitCatalog',
+     'C' => 'Catalog',
+     'O' => 'VolumeToCatalog',
+     'd' => 'DiskToCatalog',
+     'A' => 'Data'
+  );
 
   // Job types
-  $job_types = array( 
-     'B' => 'Backup',
+  $job_types = array( 'B' => 'Backup',
      'M' => 'Migrated',
      'V' => 'Verify',
      'R' => 'Restore',
      'D' => 'Admin',
      'A' => 'Archive',
      'C' => 'Copy',
-     'g' => 'Migration' );
+     'g' => 'Migration' 
+  );
  
   // Jobs type filter
   $job_type_filter = '0';
@@ -107,6 +105,7 @@
   foreach( $pools->getPools() as $pool ) {
      $pools_list[$pool['poolid']] = $pool['name'];
   }
+
   $pools_list[0] = 'Any';
 
   $view->assign('pools_list', $pools_list);
@@ -146,26 +145,26 @@
     }
 
   // Selected level filter
-    if (!is_null(CHttpRequest::get_Value('level_id'))) {
-        $level_id = CHttpRequest::get_Value('level_id');
-        $view->assign('level_filter', $level_id);
-
-        if (!is_null(CHttpRequest::get_value('status'))) {
-           if (!empty($level_id)) {
-              $jobs->addParameter( 'job_level', $level_id);
-                $query    .= "AND Job.Level = :job_level ";
-            }
-        } else {
-            if (!empty($level_id)) {
-                $query    .= "WHERE Job.Level = '$level_id' ";
-            }
+  if (!is_null(CHttpRequest::get_Value('level_id'))) {
+     $level_id = CHttpRequest::get_Value('level_id');
+     $view->assign('level_filter', $level_id);
+     
+     if (!is_null(CHttpRequest::get_value('status'))) {
+        if (!empty($level_id)) {
+           $jobs->addParameter( 'job_level', $level_id);
+           $query    .= "AND Job.Level = :job_level ";
         }
-    } else {
-        $view->assign('level_filter', '');
-    }
-
-   // Selected pool filter
-   if (!is_null(CHttpRequest::get_Value('pool_id'))) {
+     } else {
+        if (!empty($level_id)) {
+           $query    .= "WHERE Job.Level = '$level_id' ";
+        }
+     }
+  } else {
+     $view->assign('level_filter', '');
+  }
+  
+  // Selected pool filter
+  if (!is_null(CHttpRequest::get_Value('pool_id'))) {
       $pool_id = CHttpRequest::get_Value('pool_id');
       $jobs->addParameter( 'pool_id', $pool_id);
       $view->assign('pool_filter', $pool_id);
@@ -299,116 +298,118 @@
     $jobsresult = $jobs->run_query($query);
 
     foreach ($jobsresult as $job) {
-        // Determine icon for job status
-        switch($job['jobstatus']) {
-            case J_RUNNING:
-                $job['Job_icon'] = "play";
-                break;
-            case J_COMPLETED:
-                $job['Job_icon'] = "ok";
-                break;
-            case J_CANCELED:
-                $job['Job_icon'] = "off";
-                break;
-            case J_COMPLETED_ERROR:
-                $job['Job_icon'] = "warning-sign";
-                break;
-            case J_FATAL:
-                $job['Job_icon'] = "remove";
-                break;
-            case J_WAITING_CLIENT:
-            case J_WAITING_SD:
-            case J_WAITING_MOUNT_MEDIA:
-            case J_WAITING_NEW_MEDIA:
-            case J_WAITING_STORAGE_RES:
-            case J_WAITING_JOB_RES:
-            case J_WAITING_CLIENT_RES:
-            case J_WAITING_MAX_JOBS:
-            case J_WAITING_START_TIME:
-            case J_NOT_RUNNING:
-                $job['Job_icon'] = "time";
-                break;
-        } // end switch
+       // Determine icon for job status
+       switch($job['jobstatus']) {
+       case J_RUNNING:
+          $job['Job_icon'] = "play";
+          break;
+       case J_COMPLETED:
+          $job['Job_icon'] = "ok";
+          break;
+       case J_CANCELED:
+          $job['Job_icon'] = "off";
+          break;
+       case J_COMPLETED_ERROR:
+          $job['Job_icon'] = "warning-sign";
+          break;
+       case J_FATAL:
+          $job['Job_icon'] = "remove";
+          break;
+       case J_WAITING_CLIENT:
+       case J_WAITING_SD:
+       case J_WAITING_MOUNT_MEDIA:
+       case J_WAITING_NEW_MEDIA:
+       case J_WAITING_STORAGE_RES:
+       case J_WAITING_JOB_RES:
+       case J_WAITING_CLIENT_RES:
+       case J_WAITING_MAX_JOBS:
+       case J_WAITING_START_TIME:
+       case J_NOT_RUNNING:
+          $job['Job_icon'] = "time";
+          break;
+       } // end switch
+       
+       // Job start time, end time and scheduled time in custom format (if defined)
+       $job['starttime'] = date( $dbSql->datetime_format, strtotime($job['starttime']));
+       $job['endtime'] = date( $dbSql->datetime_format, strtotime($job['endtime'])); 
+       $job['schedtime'] = date( $dbSql->datetime_format, strtotime($job['schedtime'])); 
+       
+       $start_time = $job['starttime'];
+       $end_time   = $job['endtime'];
+       
+       if ($start_time == '0000-00-00 00:00:00' or is_null($start_time) or $start_time == 0) {
+          $job['starttime'] = 'n/a';
+       }
+       
+       if ($end_time == '0000-00-00 00:00:00' or is_null($end_time) or $end_time == 0) {
+          $job['endtime'] = 'n/a';
+       }
+       
+       // Get the job elapsed time completion
+       $job['elapsed_time'] = DateTimeUtil::Get_Elapsed_Time($start_time, $end_time);
+       
+       // Job Level
+       $job['level'] = $job_levels[$job['level']];
+       
+       // Job files
+       $job['jobfiles'] = CUtils::format_Number($job['jobfiles']);
 
-        // Job start time, end time and scheduled time in custom format (if defined)
-        $job['starttime'] = date( $dbSql->datetime_format, strtotime($job['starttime']));
-        $job['endtime'] = date( $dbSql->datetime_format, strtotime($job['endtime'])); 
-        $job['schedtime'] = date( $dbSql->datetime_format, strtotime($job['schedtime'])); 
-        
-        $start_time = $job['starttime'];
-        $end_time   = $job['endtime'];
+       // Set default Job speed and compression rate
+       $job['speed'] = '0 Mb/s';
+       $job['compression'] = 'n/a';
+       
+       switch($job['jobstatus']) {
+       case J_COMPLETED:
+       case J_COMPLETED_ERROR:
+       case J_NO_FATAL_ERROR:
+       case J_CANCELED:
+          // Job speed
+          $seconds = DateTimeUtil::get_ElaspedSeconds($end_time, $start_time);
 
-        if ($start_time == '0000-00-00 00:00:00' or is_null($start_time) or $start_time == 0) {
-            $job['starttime'] = 'n/a';
-        }
-
-        if ($end_time == '0000-00-00 00:00:00' or is_null($end_time) or $end_time == 0) {
-            $job['endtime'] = 'n/a';
-        }
-
-        // Get the job elapsed time completion
-        $job['elapsed_time'] = DateTimeUtil::Get_Elapsed_Time($start_time, $end_time);
-
-        // Job Level
-        $job['level'] = $job_levels[$job['level']];
-
-        // Job files
-        $job['jobfiles'] = CUtils::format_Number($job['jobfiles']);
-
-        // Set default Job speed and compression rate
-        $job['speed'] = '0 Mb/s';
-        $job['compression'] = 'n/a';
-
-        switch($job['jobstatus']) {
-            case J_COMPLETED:
-            case J_COMPLETED_ERROR:
-            case J_NO_FATAL_ERROR:
-            case J_CANCELED:
-                // Job speed
-                $seconds = DateTimeUtil::get_ElaspedSeconds($end_time, $start_time);
-                if ($seconds !== false && $seconds > 0) {
-                    $speed     = $job['jobbytes'] / $seconds;
-                    $speed     = CUtils::Get_Human_Size($speed, 2) . '/s';
-                    $job['speed'] = $speed;
-                } else {
-                    $job['speed'] = 'n/a';
-                }
-
-                // Job compression
-                if ($job['jobbytes'] > 0 && $job['type'] == 'B') {
-                    $compression        = (1-($job['jobbytes'] / $job['readbytes']));
-                    $job['compression'] = number_format($compression, 2);
-                } else {
-                    $job['compression'] = 'n/a';
-                }
-            break;
-        } // end switch
-
-        // Job size
-        $job['jobbytes'] = CUtils::Get_Human_Size($job['jobbytes']);
-
-        // Job Pool
-        if (is_null($job['pool_name'])) {
-            $job['pool_name'] = 'n/a';
-        }
-
-        $last_jobs[] = $job;
+          if ($seconds !== false && $seconds > 0) {
+             $speed     = $job['jobbytes'] / $seconds;
+             $speed     = CUtils::Get_Human_Size($speed, 2) . '/s';
+             $job['speed'] = $speed;
+          } else {
+             $job['speed'] = 'n/a';
+          }
+          
+          // Job compression
+          if ($job['jobbytes'] > 0 && $job['type'] == 'B') {
+             $compression        = (1-($job['jobbytes'] / $job['readbytes']));
+             $job['compression'] = number_format($compression, 2);
+          } else {
+             $job['compression'] = 'n/a';
+          }
+          break;
+       } // end switch
+       
+       // Job size
+       $job['jobbytes'] = CUtils::Get_Human_Size($job['jobbytes']);
+       
+       // Job Pool
+       if (is_null($job['pool_name'])) {
+          $job['pool_name'] = 'n/a';
+       }
+       
+       $last_jobs[] = $job;
+    
     } // end foreach
-
+    
     $view->assign('last_jobs', $last_jobs);
-
-  // Count jobs
+    
+    // Count jobs
     $view->assign('jobs_found', count($last_jobs));
-
-  // Set page name
+    
+    // Set page name
     $view->assign('page_name', 'Jobs report');
-
+    
     // Language
     $view->assign('config_language', FileConfig::get_Value('language'));
-
-  // Process and display the template
+    
+    // Process and display the template
     $view->render('jobs.tpl');
 
-  } catch (Exception $e) {
-         CErrorHandler::displayError($e);
-  }
+} catch (Exception $e) {
+   CErrorHandler::displayError($e);
+}
