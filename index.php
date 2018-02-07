bacula-web/bacula-web

commit b2731442328f2fe48eaa087ec48b14a651dec66d

Remove catalog statistics from main dashboard
 - catalog statistics can be found in Reports / Director(s) report page from
   now

index.php
@@ -29,6 +29,8 @@ try {
     $catalog   = new Database_Model();
     $clients   = new Clients_Model(); 
     $filesets  = new FileSets_Model();
+    $pools = new Pools_Model();
+    $volumes = new Volumes_Model();
 
     require_once('core/const.inc.php');
 
@@ -83,39 +85,10 @@ try {
     // Stored files number
     $view->assign('stored_files', CUtils::format_Number($jobs->getStoredFiles($no_period)));
  
-    // Overall stored bytes
-    $view->assign('stored_bytes', CUtils::Get_Human_Size($jobs->getStoredBytes($no_period)));
-
-    // Database size
-    $view->assign('database_size', $catalog->get_Size( $dbSql->catalog_current_id ));
- 
-    // Total bytes and files stored over the last 24 hours
-    $view->assign('bytes_last', CUtils::Get_Human_Size($jobs->getStoredBytes($custom_period)));
-    $view->assign('files_last', CUtils::format_Number($jobs->getStoredFiles($custom_period)));
-
-    // Number of clients
-    $view->assign('clients', $clients->count());
-
-    // Defined Jobs and Filesets
-    $view->assign('defined_filesets', $filesets->count());
-    $view->assign('defined_jobs', $jobs->count_Job_Names());
-
     // Incremental, Differential and Full jobs over the last 24 hours
     $view->assign('incr_jobs', $jobs->count_Jobs( $custom_period, null, J_INCR));
     $view->assign('diff_jobs', $jobs->count_Jobs( $custom_period, null, J_DIFF));
     $view->assign('full_jobs', $jobs->count_Jobs( $custom_period, null, J_FULL));
-
-    // Volumes disk usage
-    $volumes = new Volumes_Model();
-    $volumes_size = $volumes->getDiskUsage();
-    $view->assign('volumes_size', CUtils::Get_Human_Size($volumes_size));
-
-    // Pools count
-    $pools = new Pools_Model();
-    $view->assign('pools_nb', $pools->count());
-
-    // Count volumes
-    $view->assign('volumes_nb', $volumes->count());
 
     // ==============================================================
     // Last period <Job status graph>

application/view/index.tpl
@@ -3,85 +3,7 @@
 <div class="container-fluid">
 	<h3>{$page_name}</h3>
 	
-	<!-- First row with catalog statistics -->
-	<h4>{t}Catalog statistics{/t}</h4>
-	
-	<div class="row">
-	  <!-- Defined clients -->
-	  <div class="col-xs-6 col-sm-4 col-lg-2 col-lg-offset-1">
-	    <div class="panel panel-default">
-	      <div class="panel-heading">{t}Clients{/t}</div>
-		  <div class="panel-body text-right"><h3>{$clients}</h3></div>
-	    </div> <!-- end <div class="panel panel-default"> -->
-	  </div> <!-- end <div class="col-xs-2 col-xs-offset-1"> -->
-	
-	  <!-- Defined Jobs -->
-	  <div class="col-xs-6 col-sm-4 col-lg-2">
-	    <div class="panel panel-default">
-		  <div class="panel-heading">{t}Jobs{/t}</div>
-		  <div class="panel-body text-right"><h3>{$defined_jobs}</h3></div>
-		</div> <!-- end <div class="panel ... -->
-	  </div> <!-- end <div class="col- -->
-	  
-	  <!-- Defined FileSets -->
-	  <div class="col-xs-6 col-sm-4 col-lg-2">
-	    <div class="panel panel-default">
-	      <div class="panel-heading">{t}Filesets{/t}</div>
-		  <div class="panel-body text-right"><h3>{$defined_filesets}</h3></div>
-	    </div>
-	  </div>
-	  
-	  <!-- Stored bytes -->
-	  <div class="col-xs-6 col-sm-4 col-lg-2">
-	    <div class="panel panel-default">
-	      <div class="panel-heading">{t}Total bytes{/t}</div>
-		  <div class="panel-body text-right"><h3>{$stored_bytes}</h3></div>
-		</div>
-	  </div>
-	  
-	  <!-- Stored files -->
-	  <div class="col-xs-12 col-sm-4 col-lg-2">
-	    <div class="panel panel-default">
-	      <div class="panel-heading">{t}Total files{/t}</div>
-		  <div class="panel-body text-right"><h3>{$stored_files}</h3></div>
-		</div>
-	  </div>
-	
-	  <!-- Catalog (database) size -->
-      <div class="col-xs-6 col-sm-4 col-lg-2 col-lg-offset-1">    
-	    <div class="panel panel-default">
-		  <div class="panel-heading">{t}Database size{/t}</div>
-		  <div class="panel-body text-right"><h3>{$database_size}</h3></div>
-		</div> <!-- end <div class="panel ...." -->
-	  </div> <!-- end <div class="col-..." -->
-
-      <!-- Defined pools -->
-	  <div class="col-xs-6 col-sm-4 col-lg-2">
-	    <div class="panel panel-default">
-		  <div class="panel-heading">{t}Pool(s){/t}</div>
-		  <div class="panel-body text-right"><h3>{$pools_nb}</h3></div>
-		</div> <!-- end <div class="panel ...." -->
-	  </div> <!-- end <div class="col-..." -->
-	  
-	  <!-- Defined volumes -->
-	  <div class="col-xs-6 col-sm-4 col-lg-2">
-	    <div class="panel panel-default">
-		  <div class="panel-heading">{t}Volume(s){/t}</div>
-		  <div class="panel-body text-right"><h3>{$volumes_nb}</h3></div>
-		</div> <!-- end <div class="panel ...." -->
-	  </div> <!-- end <div class="col-..." -->
-	  
-	  <!-- Volumes storage usage -->
-	  <div class="col-xs-6 col-sm-4 col-lg-2">
-	    <div class="panel panel-default">
-		  <div class="panel-heading">{t}Volume(s) size{/t}</div>
-		  <div class="panel-body text-right"><h3>{$volumes_size}</h3></div>
-		</div> <!-- end <div class="panel ...." -->
-	  </div> <!-- end <div class="col-..." -->
-
-	</div> <!-- end <div class="row equalwidth"> -->
-	
-	<!-- Second row with Jobs statistics, stored bytes and stored files widgets -->
+	<!-- First row with Jobs statistics, stored bytes and stored files widgets -->
 	<div class="row">
 		<!-- Last period job status -->
 		<div class="col-xs-12 col-md-6">

commit a0560343e573e99c951389e8848c57de22754cc8

Fix main Dashboard regression
 - fix introduced regression in commit b273144
   Transferred bytes and files for period was not displayed anymore

index.php
@@ -84,6 +84,10 @@ try {
 
     // Stored files number
     $view->assign('stored_files', CUtils::format_Number($jobs->getStoredFiles($no_period)));
+
+    // Total bytes and files stored over the last 24 hours
+    $view->assign('bytes_last', CUtils::Get_Human_Size($jobs->getStoredBytes($custom_period)));
+    $view->assign('files_last', CUtils::format_Number($jobs->getStoredFiles($custom_period)));
  
     // Incremental, Differential and Full jobs over the last 24 hours
     $view->assign('incr_jobs', $jobs->count_Jobs( $custom_period, null, J_INCR));