26b891fe47581721bd9a837ca01e73a05b258530
Davide Franco <bacula-dev@dflc.ch>
2018-02-08 12:29:46 +0100

    Add application config file

---
+++ application/config/application.php
@@ -0,0 +1,7 @@
+<?php
+
+$app = [ 'name' => 'Bacula-Web', 'version' => '8.0.0-rc1',
+    'routes' => [   'home' => 'Dashboard', 
+                    'test' => 'Test' ],
+                    'about' => 'About',
+    'defaultview' => 'DashboardView' ];
