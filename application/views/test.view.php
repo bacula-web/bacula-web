d551f8777b21cde96a15351c331d5a51c818773b
Davide Franco <bacula-dev@dflc.ch>
2018-02-09 12:22:05 +0100

    Test page script now extend CView class

--- application/views/test.view.php
+++ application/views/test.view.php
@@ -17,139 +17,139 @@
  +-------------------------------------------------------------------------+
 */
 
-session_start();
-require_once("core/global.inc.php");
+class TestView extends CView {
+    
+    public function __construct() {
+        
+        $this->templateName = 'test.tpl';
+        $this->name = 'Test page';
+        $this->title = 'Check requirements and configuration';
 
-// Initialise model and view
-$view = new CView('test.tpl');
-
-try {
-    $catalog = new Database_Model();
-    $dbSql = new Bweb($view);
-} catch (Exception $e) {
-    CErrorHandler::displayError($e);
-}
-
-// Installed PDO drivers
-$pdo_drivers = PDO::getAvailableDrivers();
-
-// Check result icon
-//$icon_result = array( true => 'ok.png', false => 'error.png');
-$icon_result = array( true => 'glyphicon-ok', false => 'glyphicon-remove');
-
-// Checks list
-$check_list = array(
-    array('check_cmd' => 'php-gettext',
-        'check_label' => 'PHP - Gettext support',
-        'check_descr' => 'If you want Bacula-web in your language, please compile PHP with Gettext support'),
-    array('check_cmd' => 'php-session',
-        'check_label' => 'PHP - Session support',
-        'check_descr' => 'PHP session support is required'),
-    array('check_cmd' => 'php-mysql',
-        'check_label' => 'PHP - MySQL support',
-        'check_descr' => 'PHP MySQL support must be installed in order to run bacula-web with MySQL bacula catalog'),
-    array('check_cmd' => 'php-postgres',
-        'check_label' => 'PHP - PostgreSQL support',
-        'check_descr' => 'PHP PostgreSQL support must be installed in order to run bacula-web with PostgreSQL bacula catalog'),
-    array('check_cmd' => 'php-sqlite',
-        'check_label' => 'PHP - SQLite support',
-        'check_descr' => 'PHP SQLite support musts be installed in order to run bacula-web with SQLite bacula catalog'),
-    array('check_cmd' => 'php-pdo',
-        'check_label' => 'PHP - PDO support',
-        'check_descr' => 'PHP PDO support is required, please compile PHP with this option'),
-    array('check_cmd' => 'db-connection',
-        'check_label' => 'Database connection status (MySQL and postgreSQL only)',
-        'check_descr' => 'Current status: ' . $catalog->getConnectionStatus() ),
-    array('check_cmd' => 'smarty-cache',
-        'check_label' => 'Smarty cache folder write permission',
-        'check_descr' => realpath(VIEW_CACHE_DIR) . ' must be writable by Apache'),
-    array('check_cmd' => 'php-version',
-        'check_label' => 'PHP version',
-        'check_descr' => 'PHP version must be at least 5.6 (current version = ' . PHP_VERSION . ')'),
-    array('check_cmd' => 'php-timezone',
-        'check_label' => 'PHP timezone',
-        'check_descr' => 'Timezone must be configured in php.ini (current timezone = ' . ini_get('date.timezone') . ')')
-);
-
-// Doing all checks
-foreach ($check_list as &$check) {
-    switch ($check['check_cmd']) {
-        case 'php-session':
-            $check['check_result'] = $icon_result[function_exists('session_start')];
-            break;
-        case 'php-gettext':
-            $check['check_result'] = $icon_result[function_exists('gettext')];
-            break;
-        case 'pear-db':
-            $check['check_result'] = $icon_result[class_exists('DB')];
-            break;
-        case 'php-mysql':
-            $check['check_result'] = $icon_result[in_array('mysql', $pdo_drivers)];
-            break;
-        case 'php-postgres':
-            $check['check_result'] = $icon_result[in_array('pgsql', $pdo_drivers)];
-            break;
-        case 'php-sqlite':
-            $check['check_result'] = $icon_result[in_array('sqlite', $pdo_drivers)];
-            break;
-        case 'php-pdo':
-            $check['check_result'] = $icon_result[class_exists('PDO')];
-            break;
-        case 'smarty-cache':
-            $check['check_result'] = $icon_result[is_writable(VIEW_CACHE_DIR)];
-            break;
-        case 'php-version':
-            $check['check_result'] = $icon_result[version_compare(PHP_VERSION, '5.6', '>=')];
-            break;
-        case 'db-connection':
-            $check['check_result'] = $icon_result[$catalog->isConnected()];
-            break;
-        case 'php-timezone':
-            $timezone = ini_get('date.timezone');
-            if (!empty($timezone)) {
-                $check['check_result'] = $icon_result[true];
-            } else {
-                $check['check_result'] = $icon_result[false];
-            }
-            break;
+        parent::init();
     }
-}
 
-// Testing graph capabilities
-$data = array(
-    array('test', 100),
-    array('test1', 150),
-    array('test2', 180),
-    array('test3', 270),
-    array('test4', 456)
- ); 
+    public function prepare() {
 
-// Dummy Pie chart
-$pie_chart = new Chart( array(   'type' => 'pie', 
-                                 'name' => 'chart_pie_test', 
-                                 'data' => $data ) );
+        $catalog = new Database_Model(); 
+        
+        // Installed PDO drivers
+        $pdo_drivers = PDO::getAvailableDrivers();
+        
+        // Check result icon
+        //$icon_result = array( true => 'ok.png', false => 'error.png');
+        $icon_result = array( true => 'glyphicon-ok', false => 'glyphicon-remove');
 
-$view->assign( 'pie_graph_id', $pie_chart->name);
-$view->assign( 'pie_graph', $pie_chart->render());
+        // Checks list
+        $check_list = array(
+            array(  'check_cmd' => 'php-gettext',
+                    'check_label' => 'PHP - Gettext support',
+                    'check_descr' => 'If you want Bacula-web in your language, please compile PHP with Gettext support'),
+            array(  'check_cmd' => 'php-session',
+                    'check_label' => 'PHP - Session support',
+                    'check_descr' => 'PHP session support is required'),
+            array(  'check_cmd' => 'php-mysql',
+                    'check_label' => 'PHP - MySQL support',
+                    'check_descr' => 'PHP MySQL support must be installed in order to run bacula-web with MySQL bacula catalog'),
+            array(  'check_cmd' => 'php-postgres',
+                    'check_label' => 'PHP - PostgreSQL support',
+                    'check_descr' => 'PHP PostgreSQL support must be installed in order to run bacula-web with PostgreSQL bacula catalog'),
+            array(  'check_cmd' => 'php-sqlite',
+                    'check_label' => 'PHP - SQLite support',
+                    'check_descr' => 'PHP SQLite support musts be installed in order to run bacula-web with SQLite bacula catalog'),
+            array(  'check_cmd' => 'php-pdo',
+                    'check_label' => 'PHP - PDO support',
+                    'check_descr' => 'PHP PDO support is required, please compile PHP with this option'),
+            array(  'check_cmd' => 'db-connection',
+                    'check_label' => 'Database connection status (MySQL and postgreSQL only)',
+                    'check_descr' => 'Current status: ' . $catalog->getConnectionStatus() ),
+            array(  'check_cmd' => 'smarty-cache',
+                    'check_label' => 'Smarty cache folder write permission',
+                    'check_descr' => realpath(VIEW_CACHE_DIR) . ' must be writable by Apache'),
+            array(  'check_cmd' => 'php-version',
+                    'check_label' => 'PHP version',
+                    'check_descr' => 'PHP version must be at least 5.6 (current version = ' . PHP_VERSION . ')'),
+            array(  'check_cmd' => 'php-timezone',
+                    'check_label' => 'PHP timezone',
+                    'check_descr' => 'Timezone must be configured in php.ini (current timezone = ' . ini_get('date.timezone') . ')')
+        );
 
-unset($pie_graph);
+        // Doing all checks
+        foreach ($check_list as &$check) {
+            switch ($check['check_cmd']) {
+            case 'php-session':
+                $check['check_result'] = $icon_result[function_exists('session_start')];
+                break;
+            case 'php-gettext':
+                $check['check_result'] = $icon_result[function_exists('gettext')];
+                break;
+            case 'pear-db':
+                $check['check_result'] = $icon_result[class_exists('DB')];
+                break;
+            case 'php-mysql':
+                $check['check_result'] = $icon_result[in_array('mysql', $pdo_drivers)];
+                break;
+            case 'php-postgres':
+                $check['check_result'] = $icon_result[in_array('pgsql', $pdo_drivers)];
+                break;
+            case 'php-sqlite':
+                $check['check_result'] = $icon_result[in_array('sqlite', $pdo_drivers)];
+                break;
+            case 'php-pdo':
+                $check['check_result'] = $icon_result[class_exists('PDO')];
+                break;
+            case 'smarty-cache':
+                $check['check_result'] = $icon_result[is_writable(VIEW_CACHE_DIR)];
+                break;
+            case 'php-version':
+                $check['check_result'] = $icon_result[version_compare(PHP_VERSION, '5.6', '>=')];
+                break;
+            case 'db-connection':
+                $check['check_result'] = $icon_result[$catalog->isConnected()];
+                break;
+            case 'php-timezone':
+                $timezone = ini_get('date.timezone');
+                if (!empty($timezone)) {
+                    $check['check_result'] = $icon_result[true];
+                } else {
+                    $check['check_result'] = $icon_result[false];
+                }
+                break;
+            }
+        }
+        
+        // Testing graph capabilities
+        $data = array( array('test', 100),
+            array('test1', 150),
+            array('test2', 180),
+            array('test3', 270),
+            array('test4', 456)
+        ); 
+        
+        // Dummy Pie chart
+        $pie_chart = new Chart( array(   'type' => 'pie', 
+            'name' => 'chart_pie_test', 
+            'data' => $data ) );
+        
+        $this->assign( 'pie_graph_id', $pie_chart->name);
+        $this->assign( 'pie_graph', $pie_chart->render());
+        
+        unset($pie_graph);
+        
+        // Dummy bar graph
+        $bar_chart = new Chart( array(   'type' => 'bar', 
+            'name' => 'chart_bar_test', 
+            'data' => $data,
+            'ylabel' => 'Coffee cups' ) );
 
-// Dummy bar graph
-$bar_chart = new Chart( array(   'type' => 'bar', 
-                                 'name' => 'chart_bar_test', 
-                                 'data' => $data,
-                                 'ylabel' => 'Coffee cups' ) );
+        $this->assign('bar_chart_id', $bar_chart->name);
+        $this->assign('bar_chart', $bar_chart->render());
 
-$view->assign('bar_chart_id', $bar_chart->name);
-$view->assign('bar_chart', $bar_chart->render());
+        unset($bar_chart);
 
-unset($bar_chart);
-
-// Set page name
-$view->assign('page_name', 'Test page');
+        // Set page name
+        $this->assign('page_name', 'Test page');
  
-// Template rendering
-$view->assign('checks', $check_list);
-
-// Render the view
-$view->render();
+        // Template rendering
+        $this->assign('checks', $check_list);
+    }
+} // end TestView class
