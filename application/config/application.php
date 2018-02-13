bee4a978f1c8fe17fa8d6baf4708184e82e8d641
Davide Franco <bacula-dev@dflc.ch>
2018-02-13 07:36:57 +0100

    Update application config
     - add known routes

--- application/config/application.php
+++ application/config/application.php
@@ -1,7 +1,13 @@
 <?php
 
 $app = [ 'name' => 'Bacula-Web', 'version' => '8.0.0-rc1',
-    'routes' => [   'home' => 'Dashboard', 
-                    'test' => 'Test' ],
-                    'about' => 'About',
+        'routes' => [   'home' => 'Dashboard', 
+                        'test' => 'Test',
+                        'jobs' => 'Jobs',
+                        'joblogs' => 'JobLogs',
+                        'volumes' => 'Volumes',
+                        'pools' => 'Pools',
+                        'client' => 'Client',
+                        'backupjob' => 'BackupJob',
+                        'directors' => 'Directors'],
     'defaultview' => 'DashboardView' ];
