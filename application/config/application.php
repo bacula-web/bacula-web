38f9070f8912bf2586cc2f0b7e5ae49909095165
Davide Franco <bacula-dev@dflc.ch>
2019-01-06 20:24:05 +0100

    Update version

--- application/config/application.php
+++ application/config/application.php
@@ -5,7 +5,7 @@
  * Do not modify it unless you know what you're doing.
  */
 
-$app = [ 'name' => 'Bacula-Web', 'version' => '8.1.0',
+$app = [ 'name' => 'Bacula-Web', 'version' => '8.2.0',
         'routes' => [   'home' => 'Dashboard', 
                         'test' => 'Test',
                         'jobs' => 'Jobs',
