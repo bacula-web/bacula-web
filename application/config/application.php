4deb22a54de26e82784bb1b90d36751f39df129f
Davide Franco <bacula-dev@dflc.ch>
2019-03-14 07:50:44 +0000

    Update Bacula-Web version before release

--- application/config/application.php
+++ application/config/application.php
@@ -5,7 +5,7 @@
  * Do not modify it unless you know what you're doing.
  */
 
-$app = [ 'name' => 'Bacula-Web', 'version' => '8.2.1',
+$app = [ 'name' => 'Bacula-Web', 'version' => '8.3.0',
         'routes' => [   'home' => 'Dashboard', 
                         'test' => 'Test',
                         'jobs' => 'Jobs',
