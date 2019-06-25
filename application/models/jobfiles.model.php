02cdd8fa6d9bb056a7f1eff07144c418ff75bf10
Davide Franco <bacula-dev@dflc.ch>
2019-06-25 09:32:42 +0200

    Fix bug #39 - Error with postgreSQL using JobFiles report page
     - Fix SQL query to be compatible with postgreSQL

--- application/models/jobfiles.model.php
+++ application/models/jobfiles.model.php
@@ -14,17 +14,23 @@
 class JobFiles_Model extends CModel {
 	
 	public function getJobFiles($jobId, $limit, $offset){
-		$used_types = array();
-        $sql_query = "SELECT `Job`.`Name`, `Job`.`JobStatus`, `File`.`FileIndex`, CONCAT(`Path`.`Path`, `Filename`.`Name`) as Path 
-			FROM `File`, `Path`, `Filename`, `Job` 
-			WHERE `File`.`JobId` = $jobId 
-			AND  `Path`.`PathId` = `File`.`PathId` 
-			AND  `Filename`.`FilenameId` = `File`.`FilenameId` 
-			AND  `Job`.`JobId` = `File`.`JobId` 
-			ORDER BY `File`.`FileIndex` ASC
-            LIMIT ".($offset*$limit).",$limit ;";
+        $used_types = array();
 
-        $result = $this->run_query($sql_query);
+        $fields = array('Job.Name', 'Job.JobStatus', 'File.FileIndex', 'Path.Path', 'Filename.Name');
+        $where = array("File.JobId = $jobId");
+        $orderby = 'File.FileIndex ASC';
+        $sqlQuery = CDBQuery::get_Select( array('table' => 'File',
+            'fields' => $fields,
+            'join' =>   array(  array('table' => 'Path', 'condition' => 'Path.PathId = File.PathId'),
+                                array('table' => 'Filename', 'condition' => 'File.FilenameId = Filename.FilenameId'),
+                                array('table' => 'Job', 'condition' => 'Job.JobId = File.JobId') ),
+            'where' => $where,
+            'orderby' => $orderby,
+            'limit' => array( 'count' => $limit, 'offset' => $offset*$limit),
+            'offset' => ($offset*$limit)
+        ), $this->driver );
+
+        $result = $this->run_query($sqlQuery);
 
         $used_types = $result->fetchAll();
 
@@ -34,12 +40,12 @@ class JobFiles_Model extends CModel {
 	public function countJobFiles($jobId){
 		$used_types = array();
         $sql_query = "SELECT COUNT(*) as count
-			FROM `File`, `Path`, `Filename`, `Job` 
-			WHERE `File`.`JobId` = $jobId 
-			AND  `Path`.`PathId` = `File`.`PathId` 
-			AND  `Filename`.`FilenameId` = `File`.`FilenameId` 
-			AND  `Job`.`JobId` = `File`.`JobId` 
-			ORDER BY `File`.`FileIndex` ASC;";
+			FROM File, Path, Filename, Job 
+			WHERE File.JobId = $jobId 
+			AND  Path.PathId = File.PathId 
+			AND  Filename.FilenameId = File.FilenameId 
+            AND  Job.JobId = File.JobId; ";
+
         $result = $this->run_query($sql_query);
 
 		$used_types = $result->fetchAll();
@@ -49,9 +55,9 @@ class JobFiles_Model extends CModel {
 	
 	public function getJobNameAndJobStatusByJobId($jobId){
 		$used_types = array();
-        $sql_query = "SELECT distinct `Job`.`Name`, `Job`.`JobStatus`
-			FROM `Job` 
-			WHERE `Job`.`JobId` = $jobId;";
+        $sql_query = "SELECT distinct Job.Name, Job.JobStatus
+			FROM Job 
+			WHERE Job.JobId = $jobId;";
         $result = $this->run_query($sql_query);
 
 		$used_types = $result->fetchAll();
