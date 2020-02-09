abc13868f29b065877f3882c0f612d07b999f815
Davide Franco <bacula-dev@dflc.ch>
2020-02-09 08:17:19 +0100

    Update copyright

--- application/config/application.php
+++ application/config/application.php
@@ -1,9 +1,16 @@
 <?php
 
-/*
- * This config file contains Bacula-Web application settings
+/**
+ * Bacula-Web Core Web App config file
+ *
+ * This config file contains Bacula-Web core web application settings
  * Do not modify it unless you know what you're doing.
+ *
+ * @copyright 2010-2020 Davide Franco
+ * @author Davide Franco
+ * @since 8.0.0-rc.1
  */
+
 
 $app = [ 'name' => 'Bacula-Web', 'version' => '8.3.3',
         'routes' => [   'home' => 'Dashboard', 
