61f74ae3c9d2c39b5b6bb9343afc28c539ebe79f
Davide Franco <bacula-dev@dflc.ch>
2022-03-09 07:40:48 +0100

    Update version before release

--- application/config/application.php
+++ application/config/application.php
@@ -28,7 +28,7 @@
  * @since 8.0.0-rc.1
  */
 
-$app = [ 'name' => 'Bacula-Web', 'version' => '8.5.2',
+$app = [ 'name' => 'Bacula-Web', 'version' => '8.5.3',
         'routes' => [   'home' => 'Dashboard',
                         'test' => 'Test',
                         'jobs' => 'Jobs',
--- console.php
+++ console.php
@@ -27,7 +27,7 @@ require_once('core/global.inc.php');
 
 function printUsage()
 {
-    echo "Bacula-Web version 8.5.2\n\n";
+    echo "Bacula-Web version 8.5.3\n\n";
     echo "Usage:\n";
     echo "   php bwc [command]\n\n";
     echo "Available commands:\n";
