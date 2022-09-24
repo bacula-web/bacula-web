64040a536b94dcc2b29693c22050b02b78c4cfec
Davide Franco <bacula-dev@dflc.ch>
2022-09-24 14:08:14 +0200

    Remove useless trailing whitespace at end of line
     - fix dozens of code smell warnings :)

--- application/config/application.php
+++ application/config/application.php
@@ -2,7 +2,7 @@
 
 /**
  * Copyright (C) 2010-2022 Davide Franco
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -13,8 +13,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
