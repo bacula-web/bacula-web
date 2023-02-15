bb374ed557668c62f703b25582a13983637a1fd0
Davide Franco <bacula-dev@dflc.ch>
2023-02-15 07:28:16 +0100

    fix: Rename volumes controller
     - rename volumes controller class file

--- application/Controller/VolumesController.php
+++ application/Controller/VolumesController.php
@@ -32,7 +32,7 @@ use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 
-class VolumeController extends Controller
+class VolumesController extends Controller
 {
     /**
      * @return Response
