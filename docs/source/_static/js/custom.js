22ee1c5d2b17bc0df2b216fe5ce0a41f417a92b6
Davide Franco <bacula-dev@dflc.ch>
2024-01-02 10:53:32 +0100

    chore(docs): fix custom javascript
    Fix custom javascript

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,15 +1,15 @@
 <!-- Matomo -->
-<script>
-    var _paq = window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function() {
-    var u="https://baculaweb.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function () {
+    var u = "https://baculaweb.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u + 'matomo.php']);
     _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src='//cdn.matomo.cloud/baculaweb.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true;
+    g.src = '//cdn.matomo.cloud/baculaweb.matomo.cloud/matomo.js';
+    s.parentNode.insertBefore(g, s);
 })();
-</script>
 <!-- End Matomo Code -->
