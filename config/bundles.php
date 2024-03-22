6cb0e942d7bbe3e8056bba7d51aa37481e1e34e3
Davide Franco <bacula-dev@dflc.ch>
2024-03-22 08:28:23 +0100

    refactor: replace slimphp by symfony framework
    Replace SlimPHP by Symfony framework.
    
    Remove unused old helpers scripts and classe files.
    
    Remove several unused .htaccess files.
    
    chore(docs): update web server configuration
      Update Apache and Nginx web server configuration.
    
    chore(ci): update release workflow
      Update GitHub release workflow.
    
    docs: update third-party dependencies
      Update third-party dependencies licenses in documentation.

---
+++ config/bundles.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+];
