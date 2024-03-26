7ef05db674b5878168d88ba588696833d4417fc2
Davide Franco <bacula-dev@dflc.ch>
2024-03-26 07:52:28 +0100

    refactor: use symfony webpack encore
    Simplify public assets setup by using Symfony Webpack Encore.

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+import './styles/app.scss'
+import * as bootstrap from 'bootstrap'
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/brands'
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
