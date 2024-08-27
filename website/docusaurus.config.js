3a82a90812e62b8903a50c7fe23d01d0f497e233
Davide Franco <bacula-dev@dflc.ch>
2024-08-27 07:15:07 +0200

    fix: website url
    Fix website url in configuration.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
     favicon: 'favicons/favicon.ico',
 
     // Set the production url of your site here
-    url: 'https://new.bacula-web.org',
+    url: 'https://www.bacula-web.org',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
